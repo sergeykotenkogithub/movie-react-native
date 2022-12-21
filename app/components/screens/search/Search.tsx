946ae3a496099b275adde2834243cf4c3e76943f
sergeykotenko <sergeykotenko@bk.ru>
2022-12-21 18:49:04 +0300

    Feat(Add Search)

--- app/components/screens/search/Search.tsx
+++ app/components/screens/search/Search.tsx
@@ -1,11 +1,30 @@
 import { FC } from 'react'
 import { Text, View } from 'react-native'
 
+import { Field, Heading, Layout, Loader, MovieCatalog } from '@/components/ui'
+
+import { ISearchFormData } from './search.interface'
+import { useSearch } from './useSearch'
+
 const Search: FC = () => {
+	const { searchTerm, isLoading, control, movies } = useSearch()
 	return (
-		<View>
-			<Text>Search</Text>
-		</View>
+		<Layout isHasPadding>
+			<Heading title='Search' />
+			<View className='mt-3'>
+				<Field<ISearchFormData>
+					placeholder='Type something..'
+					control={control}
+					name='searchTerm'
+					keyboardType='web-search'
+				/>
+				{!!searchTerm ? (
+					<View className='mt-3'>
+						{isLoading ? <Loader /> : <MovieCatalog title='' movies={movies} />}
+					</View>
+				) : null}
+			</View>
+		</Layout>
 	)
 }
 
