29747af05afa5a589fdea9283e1b492d48be25a2
sergeykotenko <sergeykotenko@bk.ru>
2022-12-23 12:56:30 +0300

    Feat(Add Statistics in admin panel)

--- app/components/screens/admin/home/Admin.tsx
+++ app/components/screens/admin/home/Admin.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react'
-import { Text, View } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
 
 import { AdminNavigation, Layout } from '@/components/ui'
+
+import Statistics from './statistics/Statistics'
 
 const Admin: FC = () => {
 	return (
 		<Layout isHasPadding>
 			<AdminNavigation title='Statistics' />
+			<ScrollView showsVerticalScrollIndicator={false}>
+				<Statistics />
+			</ScrollView>
 		</Layout>
 	)
 }
